fff3928306dd052f5982fa39bb674bb041d385e4
-- <-->
2017-02-06 17:21:04 -0600

    gym app v1 added to repo

---
+++ GymApp/app/src/main/java/com/example/jarrm5/gymapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.jarrm5.gymapp;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.add_workout:
+                AlertDialog.Builder adb = new AlertDialog.Builder(this);
+                adb.setTitle("Enter the workout name");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
