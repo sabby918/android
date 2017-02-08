39ac24a6b3f9d1cf49a37c34f08e8676ca95e2d4
-- <-->
2017-02-08 12:41:39 -0600

    Close to getting user input

--- GymApp/app/src/main/java/com/example/jarrm5/gymapp/MainActivity.java
+++ GymApp/app/src/main/java/com/example/jarrm5/gymapp/MainActivity.java
@@ -17,8 +17,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-//Need the implements part to be able to respond to the dialog box
-public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,40 +34,33 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
     }
 
     @Override
+    //Clicking add workout button in the action bar
     public boolean onOptionsItemSelected(MenuItem item) {
 
         //Create the user input xml file into a java object; capturing the user input from the dialog box
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View view = inflater.inflate(R.layout.userinput, null);
-        EditText userInputText = (EditText)view.findViewById(R.id.workout_name_input);
+        //inflate means "fill"
+        View view = LayoutInflater.from(MainActivity.this)
+                .inflate(R.layout.userinput, null);
+        final EditText mAddWorkout = (EditText)view.findViewById(R.id.workout_name_input);
 
         //Creating the dialog box for entering the workout name
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Enter the workout name");
-        builder.setPositiveButton("OK",this); //Second parameter pass in which event listener should trigger when the button is clicked
-        builder.setNegativeButton("Cancel",this);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String name = mAddWorkout.getText().toString();
+                boolean brkpt = true;
+            }
+        }); //Second parameter pass in which event listener should trigger when the button is clicked
+        builder.setNegativeButton("Cancel",null);
         builder.setView(R.layout.userinput);
         builder.show();
+
+
         return true;
 
 
 
-    }
-
-    @Override
-    public void onClick(DialogInterface dialogInterface, int which) {
-
-        EditText result = (EditText) findViewById(R.id.workout_name_input);
-
-        switch (which){
-            case DialogInterface.BUTTON_POSITIVE:
-                //Get the user input here
-                int test = 9;
-                break;
-            case DialogInterface.BUTTON_NEGATIVE:
-                int test2 = 9;
-                break;
-            default:
-        }
     }
 }
